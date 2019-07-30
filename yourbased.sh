a77e3137369076d6cf11dd384aa5c4f006d90190
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-07-30 14:28:09 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
+echo "Etc/UTC" > /etc/timezone    
+dpkg-reconfigure -f noninteractive tzdata
 apt-get install -y libmpc-dev libmpfr-dev libgmp-dev libatlas-base-dev liblapack-dev gfortran graphviz texlive-xetex texlive-fonts-recommended fonts-freefont-otf latexmk lmodern
