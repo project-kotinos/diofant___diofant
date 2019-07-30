0d0811f967b104d74f2deaf5943c8ba2049f54a6
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-07-30 13:59:36 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-apt-get install libmpc-dev libmpfr-dev libgmp-dev libatlas-base-dev liblapack-dev gfortran graphviz texlive-xetex texlive-fonts-recommended fonts-freefont-otf latexmk lmodern
+apt-get install -y libmpc-dev libmpfr-dev libgmp-dev libatlas-base-dev liblapack-dev gfortran graphviz texlive-xetex texlive-fonts-recommended fonts-freefont-otf latexmk lmodern
