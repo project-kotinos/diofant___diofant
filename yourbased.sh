231b4b87377051ab8003160c8b365da82e905125
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-07-30 17:13:44 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -3,7 +3,7 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y libmpc-dev libmpfr-dev libgmp-dev libatlas-base-dev liblapack-dev gfortran graphviz texlive-xetex texlive-fonts-recommended fonts-freefont-otf latexmk lmodern
-python3 -m ensurepip
+python -m ensurepip
 pip uninstall -y pytest
 pip install --pre -U gmpy2
 pip install .[exports,plot,interactive,develop,gmpy,docs]
